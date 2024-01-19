01aaed980036af16807d2bdd1fbd9e3e591f3ecc
Kurt LaVacque <lavacquek@icloud.com>
2024-01-19 00:51:03 -0600

    Saving

--- main.cpp
+++ main.cpp
@@ -1,47 +1,14 @@
 #include <avr/sleep.h>
-#include <avr/power.h>
-#include <avr/wdt.h>
-#include <avr/io.h>
-#include <avr/interrupt.h>  // Include this for sei()
 
 
 int main()
 {
-
   while (1)
   {
-    // Disable the Watchdog Timer
-    MCUSR &= ~(1 << WDRF);
-    wdt_disable();
-    // Configure all I/O pins as input with pull-up enabled
-    DDRB = 0x00;  // Set all pins on Port B as input
-    // PORTB = 0xFF; // Enable pull-up resistors on all Port B pins
-
-    // Disable digital input buffers
-    DIDR0 = 0xFF;
-
-    // Set the sleep mode to Power-down
+    // Set sleep mode to POWER DOWN mode
     set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-
-    // // Disable ADC
-    // ADCSRA &= ~(1 << ADEN);
-
-    // Activate PRR (Power Reduction Register)
-    PRR |= (1 << PRTIM1) | (1 << PRTIM0) | (1 << PRUSI) | (1 << PRADC);
-
-    // Disable the BOD
-    sleep_bod_disable();
-
-    // Enable global interrupts
-    sei();
-
-    // Put the device to sleep
+    // enter sleep
     sleep_mode();
-
-    // The program will continue from here after wakeup
-
-    // Disable sleep mode
-    sleep_disable();
   }
   return 0;
 }
