5751529644b9193c4e1ded556f488af1a73489e4
Kurt LaVacque <lavacquek@icloud.com>
2024-01-23 23:25:47 -0600

    Update timings in Helios/Timings.h

--- Helios/Timings.h
+++ Helios/Timings.h
@@ -8,38 +8,38 @@
 // are in milliseconds.
 
 // Basic Strobe
-#define STROBE_ON_DURATION 6
-#define STROBE_OFF_DURATION 6
+#define STROBE_ON_DURATION          6
+#define STROBE_OFF_DURATION         6
 
 // Hyperstrobe
-#define HYPERSTROBE_ON_DURATION 16
-#define HYPERSTROBE_OFF_DURATION 20
+#define HYPERSTROBE_ON_DURATION     16
+#define HYPERSTROBE_OFF_DURATION    20
 
 // Dops
-#define PICOSTROBE_ON_DURATION 6
-#define PICOSTROBE_OFF_DURATION 40
+#define PICOSTROBE_ON_DURATION      6
+#define PICOSTROBE_OFF_DURATION     40
 
 // Dopy
-#define DOPS_ON_DURATION 1
-#define DOPS_OFF_DURATION 10
+#define DOPS_ON_DURATION            1
+#define DOPS_OFF_DURATION           10
 
 // Ultradops
-#define ULTRADOPS_ON_DURATION 1
-#define ULTRADOPS_OFF_DURATION 3
+#define ULTRADOPS_ON_DURATION       1
+#define ULTRADOPS_OFF_DURATION      3
 
 // Strobie
-#define STROBIE_ON_DURATION 2
-#define STROBIE_OFF_DURATION 28
+#define STROBIE_ON_DURATION         2
+#define STROBIE_OFF_DURATION        28
 
 // Signal
-#define SIGNAL_ON_DURATION 10
-#define SIGNAL_OFF_DURATION 296
+#define SIGNAL_ON_DURATION          10
+#define SIGNAL_OFF_DURATION         296
 
 // A blink speed good for blends
-#define BLEND_ON_DURATION 2
-#define BLEND_OFF_DURATION 13
+#define BLEND_ON_DURATION           2
+#define BLEND_OFF_DURATION          13
 
 // Ribbon
-#define RIBBON_DURATION 6
+#define RIBBON_DURATION             6
 
 #endif
