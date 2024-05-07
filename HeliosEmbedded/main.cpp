18bd50f56ce1e3c18925e6e08d8503486512aaae
Dan <UnrealDanMail@gmail.com>
2024-05-06 20:37:25 -0700

    okay it works

--- HeliosEmbedded/main.cpp
+++ HeliosEmbedded/main.cpp
@@ -8,19 +8,9 @@
 int main(int argc, char *argv[])
 {
   Helios::init();
-  // the main thread just initializes Helios then either spins or sleeps while
-  // the interrupt handler that Helios::init() registered calls Helios::tick()
+  // the main thread just initializes Helios then continuously calls tick
   while (Helios::keep_going()) {
-    // just sleep here
-    // TODO: test whether this is actually necessary or makes any difference
-    //       I don't know if the while-loop is actually necessary or not if
-    //       we end up sleeping here, and if we do sleep here, I don't know which
-    //       mode of sleep would produce the best power draw - or whether or not
-    //       that sleep call needs to be inside a while-loop or we can just call
-    //       it once? Will need to try various different things here and see if it
-    //       effects power draw.
-    set_sleep_mode(SLEEP_MODE_IDLE);
-    sleep_mode();
+    Helios::tick();
   }
   return 0;
 }
