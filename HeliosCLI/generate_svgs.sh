08a491453165173a21ce0235e4c1f084ad2e3626
Kurt LaVacque <lavacquek@icloud.com>
2024-09-19 18:23:59 -0500

    Added svg support

--- HeliosCLI/generate_svgs.sh
+++ HeliosCLI/generate_svgs.sh
@@ -3,7 +3,7 @@
 # Directory containing BMP files
 BMP_FOLDER="bmp_patterns"
 # Output directory for PNG files
-OUTPUT_FOLDER="circular_patterns_png"
+OUTPUT_FOLDER="circular_patterns_svg"
 
 # Create output directory if it does not exist
 mkdir -p "$OUTPUT_FOLDER"
@@ -11,9 +11,9 @@ mkdir -p "$OUTPUT_FOLDER"
 # Loop through all BMP files in the BMP_FOLDER
 for bmp_file in "$BMP_FOLDER"/*.bmp; do
     filename=$(basename -- "$bmp_file")
-    output_filename="${filename%.bmp}.png"
+    output_filename="${filename%.bmp}.svg"
     output_path="$OUTPUT_FOLDER/$output_filename"
 
     # Call the Python script to generate PNG from BMP
-    python convert_bmp_to_png.py "$bmp_file" "$output_path"
+    python convert_bmp_to_svg.py "$bmp_file" "$output_path"
 done
